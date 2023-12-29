d63aa0115af6afbcf434c8ee4ca8a9cae654cc8f
Nils Ottosson <info@noidea.se>
2023-12-29 01:28:39 +0100

    create search funk

--- SJ23POSU_Objektorienterad/SJ23POSU_Obj_Grp01/tourDiSki/src/application/controller/MgnPerticipantController.java
+++ SJ23POSU_Objektorienterad/SJ23POSU_Obj_Grp01/tourDiSki/src/application/controller/MgnPerticipantController.java
@@ -2,6 +2,7 @@ package application.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import application.model.Participant;
 import application.services.participantHandler;
@@ -21,159 +22,161 @@ import javafx.scene.text.Text;
 
 public class MgnPerticipantController {
 
-    @FXML
-    private Button btAdd;
+	@FXML
+	private Button btAdd;
 
-    @FXML
-    private Button btExit;
+	@FXML
+	private Button btExit;
 
-    @FXML
-    private Button btRandom;
+	@FXML
+	private Button btRandom;
 
-    @FXML
-    private Button btremove;
-    
-    @FXML
-    private Button btGo;
+	@FXML
+	private Button btremove;
 
+	@FXML
+	private Button btGo;
 
-    @FXML
-    private TableColumn<Participant, Integer> colPartvipantID;
+	@FXML
+	private TableColumn<Participant, Integer> colPartvipantID;
 
-    @FXML
-    private TableColumn<Participant, String> colPartvipantName;
-    
-    @FXML
-    private TableColumn<Participant, Integer> colPartvipantPosition;
+	@FXML
+	private TableColumn<Participant, String> colPartvipantName;
 
+	@FXML
+	private TableColumn<Participant, Integer> colPartvipantPosition;
 
-    @FXML
-    private TextField inputFCount;
-    
-    @FXML
-    private Text txtId;
+	@FXML
+	private TextField inputFCount;
 
-    @FXML
-    private Text txtName;
+	@FXML
+	private Text txtId;
 
-    @FXML
-    private Text txtPosition;
+	@FXML
+	private Text txtName;
 
-    @FXML
-    private Text txtStartTime;
-    
-    @FXML
-    private Text txtDiffrenceTime;
-    
-    @FXML
-    private Text txtEndTime;
-    
-    @FXML
-    private Text txtRace1;
+	@FXML
+	private Text txtPosition;
 
-    @FXML
-    private Text txtRace2;
+	@FXML
+	private Text txtStartTime;
 
-    @FXML
-    private Text txtRace3;
-    
-    @FXML
-    private TextField txSearchFeild;
+	@FXML
+	private Text txtDiffrenceTime;
 
-    
-    @FXML
-    private TextField txfRadomAdd;
+	@FXML
+	private Text txtEndTime;
 
-    @FXML
-    private TableView<Participant> tbplist;
+	@FXML
+	private Text txtRace1;
 
-    @FXML
-    private TextField txFieldName;
-    
-    participantHandler parHandler = new participantHandler();
-    
-    ObservableList<Participant> list = FXCollections.observableArrayList();
-    
-    
-    
-    @FXML
-    void mgnPartAdd(ActionEvent event) {
-    	list = parHandler.addRanPercitipant(list,txFieldName.getText());
-    	
-    	
-    	
-    	System.out.println(list);
-    	
-    	colPartvipantID.setCellValueFactory(new PropertyValueFactory<Participant, Integer>("id"));
+	@FXML
+	private Text txtRace2;
+
+	@FXML
+	private Text txtRace3;
+
+	@FXML
+	private TextField txSearchFeild;
+
+	@FXML
+	private TextField txfRadomAdd;
+
+	@FXML
+	private TableView<Participant> tbplist;
+
+	@FXML
+	private TextField txFieldName;
+
+	participantHandler parHandler = new participantHandler();
+
+	ObservableList<Participant> list = FXCollections.observableArrayList();
+
+	@FXML
+	void mgnPartAdd(ActionEvent event) {
+		list = parHandler.addRanPercitipant(list, txFieldName.getText());
+
+		colPartvipantID.setCellValueFactory(new PropertyValueFactory<Participant, Integer>("id"));
 		colPartvipantName.setCellValueFactory(new PropertyValueFactory<Participant, String>("namen"));
 		colPartvipantPosition.setCellValueFactory(new PropertyValueFactory<Participant, Integer>("position"));
 
 		tbplist.setItems(list);
-		
+
 		txFieldName.setText("");
-    	
-    }
 
-    @FXML
-    void mgnPartExit(ActionEvent event) {
-    	
-    }
+	}
 
-    @FXML
-    void mgnPartRadomAdd(ActionEvent event) {
-    	int count = 0;
-    	if(!txfRadomAdd.getText().isEmpty()) {
-    		
+	@FXML
+	void mgnPartExit(ActionEvent event) {
+
+	}
+
+	@FXML
+	void mgnPartRadomAdd(ActionEvent event) {
+		int count = 0;
+		if (!txfRadomAdd.getText().isEmpty()) {
+
 			try {
 				count = Integer.parseInt(txfRadomAdd.getText());
 			} catch (NumberFormatException e) {
 				e.printStackTrace();
-				txfRadomAdd.setText(null);    
-				
+				txfRadomAdd.setText(null);
+
 			}
-    		try {
+			try {
 				list = parHandler.addRanPercitipantfromFile(list, count);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    		
-    		colPartvipantID.setCellValueFactory(new PropertyValueFactory<Participant, Integer>("id"));
-    		colPartvipantName.setCellValueFactory(new PropertyValueFactory<Participant, String>("namen"));
-    		colPartvipantPosition.setCellValueFactory(new PropertyValueFactory<Participant, Integer>("position"));
 
-    		tbplist.setItems(list);
-    		txfRadomAdd.setText(null);    		
-    	}
+			colPartvipantID.setCellValueFactory(new PropertyValueFactory<Participant, Integer>("id"));
+			colPartvipantName.setCellValueFactory(new PropertyValueFactory<Participant, String>("namen"));
+			colPartvipantPosition.setCellValueFactory(new PropertyValueFactory<Participant, Integer>("position"));
 
-    }
+			tbplist.setItems(list);
+			txfRadomAdd.setText(null);
+		}
 
-    @FXML
-    void mgnPartRemove(ActionEvent event) {
-    	int selectID = tbplist.getSelectionModel().getSelectedIndex();
-    	tbplist.getItems().remove(selectID);
-    }
-    
-    @FXML
-    void tbCelDetail(MouseEvent event) {
-    	int selectID = tbplist.getSelectionModel().getSelectedIndex();
-    	System.out.println(tbplist.getItems().get(selectID).getId());
-    	System.err.println();
-    	txtId.setText(String.valueOf(tbplist.getItems().get(selectID).getId()));
-    	txtName.setText(tbplist.getItems().get(selectID).getNamen());
-    	txtPosition.setText(String.valueOf(tbplist.getItems().get(selectID).getPosition()));
-    	txtStartTime.setText(tbplist.getItems().get(selectID).getStartTime());
-    	
-    	txtDiffrenceTime.setText(tbplist.getItems().get(selectID).getTotalDiffrenceTime());
-    	txtEndTime.setText(tbplist.getItems().get(selectID).getEndTime());
-    	txtRace1.setText(tbplist.getItems().get(selectID).getRac1());
-    	txtRace2.setText(tbplist.getItems().get(selectID).getRac2());
-    	txtRace3.setText(tbplist.getItems().get(selectID).getRac3());
-    	
-    	
-    	
-    }
-    
-    
+	}
+
+	@FXML
+	void mgnPartRemove(ActionEvent event) {
+		int selectID = tbplist.getSelectionModel().getSelectedIndex();
+		tbplist.getItems().remove(selectID);
+	}
+
+	@FXML
+	void tbCelDetail(MouseEvent event) {
+		int selectID = tbplist.getSelectionModel().getSelectedIndex();
+		System.out.println(tbplist.getItems().get(selectID).getId());
+		System.err.println();
+		txtId.setText(String.valueOf(tbplist.getItems().get(selectID).getId()));
+		txtName.setText(tbplist.getItems().get(selectID).getNamen());
+		txtPosition.setText(String.valueOf(tbplist.getItems().get(selectID).getPosition()));
+		txtStartTime.setText(tbplist.getItems().get(selectID).getStartTime());
+
+		txtDiffrenceTime.setText(tbplist.getItems().get(selectID).getTotalDiffrenceTime());
+		txtEndTime.setText(tbplist.getItems().get(selectID).getEndTime());
+		txtRace1.setText(tbplist.getItems().get(selectID).getRac1());
+		txtRace2.setText(tbplist.getItems().get(selectID).getRac2());
+		txtRace3.setText(tbplist.getItems().get(selectID).getRac3());
+
+	}
+
+	@FXML
+	void search(ActionEvent event) {
+		System.out.println("search " + txSearchFeild.getText());
+		
+		// Create temp search list
+		ObservableList<Participant> list2 = parHandler.searchList(list, txSearchFeild.getText());
+
+		if (txSearchFeild.getText().isEmpty()) {
+			tbplist.setItems(list);
+		} else {
+			tbplist.setItems(list2);
+		}
+
+	}
 
 }
--- SJ23POSU_Objektorienterad/SJ23POSU_Obj_Grp01/tourDiSki/src/application/services/participantHandler.java
+++ SJ23POSU_Objektorienterad/SJ23POSU_Obj_Grp01/tourDiSki/src/application/services/participantHandler.java
@@ -6,65 +6,70 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import application.model.Participant;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class participantHandler {
-	
-	public ObservableList<Participant> addRanPercitipant(ObservableList<Participant> parUserList,String name) {
+
+	public ObservableList<Participant> addRanPercitipant(ObservableList<Participant> parUserList, String name) {
 		Participant pUser = new Participant();
 		int indexid = parUserList.size();
-		
-		
-		if(indexid != 0) {
-			
-			indexid = parUserList.get(indexid-1).getId();
+
+		if (indexid != 0) {
+
+			indexid = parUserList.get(indexid - 1).getId();
 		}
-		
+
 		pUser.setId(indexid + 1);
 		pUser.setNamen(name);
 		pUser.setPosition(0);
 		parUserList.add(pUser);
-		
+
 		return parUserList;
 
 	}
-	
-	public ObservableList<Participant>  addRanPercitipantfromFile( ObservableList<Participant> parUserList,int  count) throws IOException {
+
+	public ObservableList<Participant> addRanPercitipantfromFile(ObservableList<Participant> parUserList, int count)
+			throws IOException {
 		BufferedReader br = null;
-		ArrayList<String> nameList  = new ArrayList<String>();
-		//ArrayList<Participant> participantList  = new ArrayList<Participant>();
-		//HashMap<Integer, ArrayList<competitor>> participant = new HashMap<Integer, ArrayList<competitor>>();
-		
+		ArrayList<String> nameList = new ArrayList<String>();
+		// ArrayList<Participant> participantList = new ArrayList<Participant>();
+		// HashMap<Integer, ArrayList<competitor>> participant = new HashMap<Integer,
+		// ArrayList<competitor>>();
+
 		try {
-			
+
 			String line = null;
 			File file = new File("randomName.txt");
 			br = new BufferedReader(new FileReader(file));
 
-			while ((line = br.readLine()) != null) {				
+			while ((line = br.readLine()) != null) {
 				nameList.add(line);
 			}
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//System.out.println(nameList);
-		
+
+		// System.out.println(nameList);
+
 		// Random get list av contest
 		Random rand = new Random();
 		int listSize = parUserList.size();
-		
-		if(count > 20) {
+
+		if (count > 20) {
 			count = 15;
 		}
-		if(listSize != 0) {
-			
-			listSize = parUserList.get(listSize-1).getId();
+		if (listSize != 0) {
+
+			listSize = parUserList.get(listSize - 1).getId();
 		}
 		for (int i = 1; i <= count; i++) {
 			Participant competitor = new Participant();
@@ -73,10 +78,49 @@ public class participantHandler {
 			competitor.setNamen(nameList.get(rand_int1));
 			parUserList.add(competitor);
 		}
-		
+
 		return parUserList;
-		
+
 	}
+
 	
 	
+	public List<String> nameObjToStringList(ObservableList<Participant> parUserList) {
+
+		List<String> participantNameList = new ArrayList<String>();
+		for (Participant participant : parUserList) {
+			participantNameList.add(participant.getNamen());
+		}
+
+		return participantNameList;
+
+	}
+
+	
+	public ObservableList<Participant> searchList( ObservableList<Participant> parUserList,String searchWords) {
+		 
+			ObservableList<Participant> returnList =  FXCollections.observableArrayList();
+		
+		 	List<String> listOfStrings = nameObjToStringList(parUserList);
+	        List<String> searchWordsArray = Arrays.asList(searchWords.trim().split(" "));
+
+	        
+	        List<String> nameList =  listOfStrings.stream().filter(input -> {
+	            return searchWordsArray.stream().allMatch(word ->
+	                    input.toLowerCase().contains(word.toLowerCase()));
+	        }).collect(Collectors.toList());
+			
+	        
+	        for(Participant nameObj : parUserList) {
+	        	if (nameList.contains(nameObj.getNamen())) {
+	        		returnList.add(nameObj);
+	        	}
+	        }
+	        System.out.println(returnList);
+	        return returnList;
+	        
+	        
+	        
+	        
+	    }
 }
