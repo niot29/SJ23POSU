7c3397366033d9e60a1179cac1c1527ab97606e2
Nils Ottosson <info@noidea.se>
2024-02-28 10:36:33 +0100

    change tabel creation

--- SJ23POSU_Databaser/select.sql
+++ SJ23POSU_Databaser/select.sql
@@ -1,24 +1,27 @@
 
 
 -- Vilka singlar återfinns på placering 1-100, sorterade i ordning från lägst till högst(1-100)
-select * from (
-select  sum(pp.place_point) as  TotalP, t.title, a.name
+set @row_num=0;
+select  (@row_num:=@row_num+1) AS place , a.* from	
+	(select sum(pp.place_point) as  TotalP, t.title, a.name
 	from place_and_point as pp
     INNER JOIN title as t ON pp.title_id = t.title_id
     INNER JOIN artist as a ON t.artist_id = a.artist_id
     group by pp.title_id
-    order by TotalP desc limit 100) as lista order by TotalP;
+    order by TotalP desc limit 100)  a;
     
 -- Vilka singlar återfinns på placering 1-20 sorterade i ordning från lägst till högst (1-20). (av kommentera för att lista bara 20 firsta vekorna)
-select  sum(pp.place_point) as  totalp, t.title, a.name
+set @row_num=0;
+select  (@row_num:=@row_num+1) AS place , a.* 
+	from ( select sum(pp.place_point) as  totalp, t.title, a.name
 	from place_and_point as pp
     INNER JOIN title as t ON pp.title_id = t.title_id
     INNER JOIN artist as a ON t.artist_id = a.artist_id
     WHERE pp.place_point > 0 
     and pp.placement < 20
-    -- and pp.weeks < 20
+    and pp.weeks < 20
     group by pp.title_id
-    order by totalp desc limit 100;
+    order by totalp desc limit 20) a;
 
 -- Vilken (antal 1st) artist har flest singlar på listan 
 select  a.name, count(a.name) as c
