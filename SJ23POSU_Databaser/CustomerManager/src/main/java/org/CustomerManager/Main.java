ef51f4cb20468ca73329dfdb012f12a79bc86482
niot29 <50738105+niot29@users.noreply.github.com>
2024-03-12 15:20:05 +0100

    New main
    Mail with chouse on witsh user typs is loggin

--- SJ23POSU_Databaser/CustomerManager/src/main/java/org/CustomerManager/Main.java
+++ SJ23POSU_Databaser/CustomerManager/src/main/java/org/CustomerManager/Main.java
@@ -1,7 +1,58 @@
-package org.CustomerManager;
+package org.example;
+
+import org.example.View.MainAdminView;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        boolean isLoggedIn = false;
+        String adminPassword = "adminPassword";
+        String cutomerPassword = "cutomerPassword";
+        MainAdminView mainAdminView = new MainAdminView();
+
+        int choise;
+
+        while (true) {
+            System.out.println("Enter the password for the user you want to login as.:");
+            String inputPassword = scanner.next();
+            if (inputPassword.equals(adminPassword)) {
+                System.out.println("Login succeeded as admin!");
+                isLoggedIn = true;
+                choise = 1;
+                break;
+            } else if (inputPassword.equals(cutomerPassword)) {
+                System.out.println("Login succeeded as cutomer!!");
+                isLoggedIn = true;
+                choise = 1;
+                break;
+            } else {
+                System.out.println("Fel lösenord. Försök igen.");
+                choise = 0;
+            }
+        }
+        switch (choise) {
+            case 1:
+                System.out.println("Redirect to Admin Menu ...");
+                String[] menu1= { "AAA", "MANAGER ROOM", "MANAGER CUTOMER", "Exit" };
+                mainAdminView.mainScreen(menu1);
+                System.exit(0);
+                // add some method for admin
+                break;
+            case 2:
+                System.out.println("Redirect to Cutomer Menu...");
+                String[] menu2 = { "BOOKING", "MANAGER ROOM", "MANAGER CUTOMER", "Exit" };
+                mainAdminView.mainScreen(menu2);
+                System.exit(0);
+                // add some method for Customer
+                break;
+            default:
+                System.out.println("Ogiltigt val. Försök igen.");
+                break;
+        }
     }
 }
+
+
