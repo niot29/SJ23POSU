f7dacbaac6715999a38c1a2d05786d732122fdbf
Nils Ottosson <info@noidea.se>
2024-03-28 15:34:04 +0100

    update class

--- SJ23POSU_SoftwareDevelopmentProcess/gurkan/src/main/java/se/sakilagui/Model/LanguageEntity.java
+++ SJ23POSU_SoftwareDevelopmentProcess/gurkan/src/main/java/se/sakilagui/Model/LanguageEntity.java
@@ -1,2 +1,18 @@
-package se.sakilagui.Model;public class LanguageEntity {
+package se.sakilagui.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "language")
+public class LanguageEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="language_id")
+    private int id;
+    @Column(name="name",length=20)
+    private String name;
+    @Column(name="last_update")
+    private String last_update;
+
 }
