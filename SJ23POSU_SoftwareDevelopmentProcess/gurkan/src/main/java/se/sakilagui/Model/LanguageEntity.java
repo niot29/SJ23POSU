c4b58e2e1fd62a07d608015788391b65b6475173
Nils Ottosson <info@noidea.se>
2024-03-28 15:49:27 +0100

    Done LanguageEntity

--- SJ23POSU_SoftwareDevelopmentProcess/gurkan/src/main/java/se/sakilagui/Model/LanguageEntity.java
+++ SJ23POSU_SoftwareDevelopmentProcess/gurkan/src/main/java/se/sakilagui/Model/LanguageEntity.java
@@ -2,6 +2,8 @@ package se.sakilagui.Model;
 
 import jakarta.persistence.*;
 
+import java.sql.Time;
+
 @Entity
 @Table(name= "language")
 public class LanguageEntity {
@@ -13,6 +15,48 @@ public class LanguageEntity {
     @Column(name="name",length=20)
     private String name;
     @Column(name="last_update")
-    private String last_update;
+    private Time last_update;
 
+    public LanguageEntity(int id, String name, Time last_update) {
+        this.id = id;
+        this.name = name;
+        this.last_update = last_update;
+    }
+
+    public LanguageEntity() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Time getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Time last_update) {
+        this.last_update = last_update;
+    }
+
+    @Override
+    public String toString() {
+        return "LanguageEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", last_update=" + last_update +
+                '}';
+    }
 }
