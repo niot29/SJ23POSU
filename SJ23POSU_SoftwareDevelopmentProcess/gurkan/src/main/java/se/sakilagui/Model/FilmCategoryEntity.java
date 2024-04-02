e77c8ceb43772cd1a345373dfe9d91125c9146b0
Nils Ottosson <info@noidea.se>
2024-04-02 07:12:15 +0200

    FilCategoryEntity select info

--- SJ23POSU_SoftwareDevelopmentProcess/gurkan/src/main/java/se/sakilagui/Model/FilmCategoryEntity.java
+++ SJ23POSU_SoftwareDevelopmentProcess/gurkan/src/main/java/se/sakilagui/Model/FilmCategoryEntity.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 
 import java.sql.Timestamp;
 
+/*
+select f.title,f.description,c.name from film_category fc inner  join film f on fc.film_id = f.film_id
+    inner join category c  on fc.category_id = c.category_id;
+ */
 @Entity
 @Table(name = "film_category")
 public class FilmCategoryEntity {
